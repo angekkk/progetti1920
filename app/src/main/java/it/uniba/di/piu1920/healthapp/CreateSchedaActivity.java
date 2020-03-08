20c14b532ebe571002ffac9b695390092176cffb
angek <a.longo58@studenti.uniba.it>
2020-03-08 22:01:02 +0100

    Layout create_scheda completato

--- app/src/main/java/it/uniba/di/piu1920/healthapp/CreateSchedaActivity.java
+++ app/src/main/java/it/uniba/di/piu1920/healthapp/CreateSchedaActivity.java
@@ -2,6 +2,7 @@ package it.uniba.di.piu1920.healthapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -17,57 +18,70 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import it.uniba.di.piu1920.healthapp.classes.Cliente;
 import it.uniba.di.piu1920.healthapp.classes.Esercizio;
+import it.uniba.di.piu1920.healthapp.classes.SessionManager;
 import it.uniba.di.piu1920.healthapp.connect.JSONParser;
 import it.uniba.di.piu1920.healthapp.connect.TwoParamsList;
-import it.uniba.di.piu1920.healthapp.recycler.ExerciseActivity;
-import it.uniba.di.piu1920.healthapp.recycler.Item;
+
 import it.uniba.di.piu1920.healthapp.recycler.RecyclerItemListener;
 
 public class CreateSchedaActivity extends AppCompatActivity {
 
 
-    private List<Item> listhome = new ArrayList<>();
+
     private static final String TAG_SUCCESS = "success";
     JSONArray arr = null;
     List<Esercizio> lista = new ArrayList<>();
+    List<Esercizio> scheda = new ArrayList<>();
     RecyclerView rv;
     private static String url_get_bozze = "http://ddauniba.altervista.org/HealthApp/get_esercizi.php";
-
+    private static String url_inserisci_scheda = "http://ddauniba.altervista.org/HealthApp/inserisci_scheda.php";
+    ExAdapt ca;
     Spinner spinner;
     ImageButton add;
     Button ok;
-
+    LinearLayoutManager llm;
+    ConstraintLayout con;
+    int idutente;
+    SessionManager session;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_scheda_activity);
         rv = (RecyclerView) findViewById(R.id.scheda);
         rv.setHasFixedSize(true);
-        LinearLayoutManager llm = new LinearLayoutManager(this);
+         llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         rv.setLayoutManager(llm);
         spinner = findViewById(R.id.spinner);
         add = findViewById(R.id.add);
         ok = findViewById(R.id.ok);
-        new GetProdotti().execute();
-
+        con=findViewById(R.id.constraint);
+        Bundle arg = getIntent().getExtras();
+        session=new SessionManager(this);
+        idutente=arg.getInt("id");
+        new GetEsercizi().execute();
+        //Inserisco gli esercizi nella lista
+        enableSwipeToDeleteAndUndo();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -79,12 +93,20 @@ public class CreateSchedaActivity extends AppCompatActivity {
             }
         });
 
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(addEsercizioScheda((String) spinner.getSelectedItem())){
+                     ca = new ExAdapt(scheda);
+                    rv.setAdapter(ca);
+                }
+            }
+        });
+
         rv.addOnItemTouchListener(new RecyclerItemListener(getApplicationContext(), rv,
                 new RecyclerItemListener.RecyclerTouchListener() {
                     public void onClickItem(View v, int position) {
-
-                        Bundle x = new Bundle();
-
+                        new inserisci_scheda().execute();
                         // Intent i = new Intent(GestioneClientiActivity.this, DetailsActivity.class);
                         // x.putSerializable("ogg",lista.get(position));
                         // i.putExtra("bund",x);
@@ -97,6 +119,19 @@ public class CreateSchedaActivity extends AppCompatActivity {
                     }
                 }));
 
+    }
+
+    boolean addEsercizioScheda(String nome){ //metodo per controllare l'esercizio nella list LISTA, e aggiungerlo nella list SCHEDA
+        boolean aggiunto=false;
+        for(int i=0;i<lista.size();i++){
+            if(lista.get(i).getNome().contentEquals(nome)){
+                if(!scheda.contains(lista.get(i))){
+                    scheda.add(lista.get(i));
+                    aggiunto= true;
+                }
+            }
+        }
+        return aggiunto;
     }
 
     @Override
@@ -112,8 +147,142 @@ public class CreateSchedaActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void enableSwipeToDeleteAndUndo() {
+        SwipeToDeleteCallback swipeToDeleteCallback = new SwipeToDeleteCallback(this) {
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
 
-    class GetProdotti extends AsyncTask<String, String, String> {
+
+                final int position = viewHolder.getAdapterPosition();
+                final Esercizio item = ca.getData().get(position);
+
+                ca.removeItem(position);
+
+
+                Snackbar snackbar = Snackbar.make(con, "Item was removed from the list.", Snackbar.LENGTH_LONG);
+                snackbar.setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        ca.restoreItem(item, position);
+                        rv.scrollToPosition(position);
+                    }
+                });
+
+                snackbar.setActionTextColor(Color.YELLOW);
+                snackbar.show();
+
+            }
+        };
+
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeToDeleteCallback);
+        itemTouchhelper.attachToRecyclerView(rv);
+    }
+
+    class inserisci_scheda extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        protected String doInBackground(String... args) {
+            TwoParamsList params = new TwoParamsList();
+            /// params.add("tipo", ""+1);
+            params.add("idu",""+ idutente);
+            params.add("id",""+ session.getUserDetails().getId());
+
+            String ret=null;
+            JSONObject json = new JSONParser().makeHttpRequest(url_inserisci_scheda, JSONParser.POST, params);
+            if (json != null) {
+                try {
+                    int success = json.getInt("success");
+                    if (success == 0) {
+                        ret= "inserito";
+                    } else if (success == -1) {
+                    }
+                } catch (JSONException e) {
+                }
+            } else {
+            }
+            return ret;
+        }
+
+        protected void onPostExecute(final String file_url) {
+            runOnUiThread(new Runnable() {
+                public void run() {
+
+                }
+            });
+        }
+    }
+
+
+    public class ExAdapt extends RecyclerView.Adapter<ExAdapt.MyViewHolder> {
+
+        private List<Esercizio> listahome;
+
+        /**
+         * View holder class
+         * */
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView name;
+
+
+            public MyViewHolder(View view) {
+                super(view);
+                name = (TextView) view.findViewById(R.id.name);
+
+
+
+            }
+        }
+
+
+        public List<Esercizio> getData() {
+            return listahome;
+        }
+
+        public ExAdapt(List<Esercizio> listahomet) {
+            this.listahome = listahomet;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_exercise, parent, false);
+            return new MyViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(ExAdapt.MyViewHolder holder, int position) {
+
+            Esercizio c = listahome.get(position);
+            System.out.println("Bind ["+holder+"] - Pos ["+position+"]"+c.getNome());
+            holder.name.setText(c.getNome());
+            // Picasso.with(ExerciseActivity.this).load("http://ddauniba.altervista.org/HealthApp/img/"+c.getLink()).into( holder.image);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return listahome.size();
+        }
+
+        public void removeItem(int position) {
+            listahome.remove(position);
+            notifyItemRemoved(position);
+        }
+
+        public void restoreItem(Esercizio item, int position) {
+            listahome.add(position, item);
+            notifyItemInserted(position);
+        }
+    }
+
+
+    class GetEsercizi extends AsyncTask<String, String, String> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -201,7 +370,7 @@ public class CreateSchedaActivity extends AppCompatActivity {
             @NonNull
             @Override
             public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_exercise, parent, false);
+                View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_exercise_scheda, parent, false);
                 return new MyViewHolder(v);
             }
 
