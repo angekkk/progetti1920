075c87d0109c4af3bf3991e3328f3b3c68acddb0
angek <a.longo58@studenti.uniba.it>
2020-04-24 12:17:06 +0200

    Issue 5 : Cronometro aggiunto

--- app/src/main/java/it/uniba/di/piu1920/healthapp/DetailsActivity.java
+++ app/src/main/java/it/uniba/di/piu1920/healthapp/DetailsActivity.java
@@ -1,21 +1,23 @@
 package it.uniba.di.piu1920.healthapp;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.squareup.picasso.Picasso;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+
 import it.uniba.di.piu1920.healthapp.classes.Esercizio;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -23,6 +25,25 @@ public class DetailsActivity extends AppCompatActivity {
     Esercizio ex;
     TextView categoria,esecuzione;
     ImageView img;
+    Button start, stop, reset;
+    int min, s, ms;
+    Handler h = new Handler();
+    private TextView tv;
+    private long startTime, timeMS, timeSB, update;
+    public Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            timeMS = SystemClock.uptimeMillis() - startTime;
+            update = timeSB + timeMS;
+            s = (int) (update / 1000);
+            min = s / 60;
+            s = s % 60;
+            ms = (int) (update % 1000);
+            tv.setText("" + min + ":" + String.format("%02d", s) + ":" + String.format("%03d", ms));
+            h.postDelayed(this, 0);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,10 +52,14 @@ public class DetailsActivity extends AppCompatActivity {
         img=findViewById(R.id.img);
         categoria=findViewById(R.id.categoria);
         esecuzione=findViewById(R.id.esecuzione);
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+        reset = findViewById(R.id.reset);
+        tv = findViewById(R.id.textView);
         Bundle  arg = getIntent().getExtras();
         Bundle x= arg.getBundle("bund");
         ex=(Esercizio) x.get("ogg");
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(ex.getNome());
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -61,6 +86,36 @@ public class DetailsActivity extends AppCompatActivity {
                     .setAction("Action", null).show();
         }
 
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTime = SystemClock.uptimeMillis();
+                h.postDelayed(r, 0);
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timeSB += timeMS;
+                h.removeCallbacks(r);
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTime = 0;
+                timeMS = 0;
+                timeSB = 0;
+                s = 0;
+                min = 0;
+                ms = 0;
+                h.removeCallbacks(r);
+                tv.setText("0:00:000");
+            }
+        });
     }
 
     //metodo per controllare la connessione ad internet
